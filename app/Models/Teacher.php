75cce492329b73d2222be807c6b6d55783b9c5ef
codercwp <63134308+codercwp@users.noreply.github.com>
2020-10-09 10:09:28 +0800

    蔡文品第二次提交 (#10)
    * 李忠政 (#1)
    
    * 陈淼的第一次提交 (#2)
    
    Co-authored-by: nathaniel-kk <71243650+nathaniel-kk@users.noreply.github.com>
    
    * 杜静文的第一次提交 (#4)
    
    Co-authored-by: nathaniel-kk <71243650+nathaniel-kk@users.noreply.github.com>
    
    * 张巾巾的第一次提交 (#5)
    
    * 杜静文的第一次提交 (#3)
    
    * 张巾巾的第一次提交
    
    Co-authored-by: DJWKK <71535077+DJWKK@users.noreply.github.com>
    Co-authored-by: nathaniel-kk <71243650+nathaniel-kk@users.noreply.github.com>
    
    * 蔡文品第一次提交
    
    Co-authored-by: bixuande <71496666+bixuande@users.noreply.github.com>
    Co-authored-by: Yidaaa-u <68274775+Yidaaa-u@users.noreply.github.com>
    Co-authored-by: nathaniel-kk <71243650+nathaniel-kk@users.noreply.github.com>
    Co-authored-by: DJWKK <71535077+DJWKK@users.noreply.github.com>
    Co-authored-by: Yeti <64764214+YetiSui@users.noreply.github.com>

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -9,4 +9,88 @@ class Teacher extends Model
     protected $table = "teacher";
     public $timestamps = true;
     protected $primaryKey = 'id';
+    protected $guarded = [];
+
+
+    public static function teacherAdd($request)
+    {
+        if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
+            $path = md5(time() . rand(1000, 9999)) .
+                '.' . $request->file('avatar')->getClientOriginalExtension();
+
+            $request->file('avatar')->move('./public', $path);
+
+            $data = $request->all();
+
+            $data['avatar'] = './public' . $path;
+
+            try {
+                $teacher = Teacher::create([
+                    'name' => $data['name'],
+                    'priority' => $data['priority'],
+                    't_url' => $data['avatar'],
+                ]);
+            } catch (\Exception $e) {
+                logError('错误',[$e->getMessage()]);
+            }
+            return $teacher;
+        }
+    }
+
+
+    public static function teacherAlter($request){
+        if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
+            $path = md5(time() . rand(1000, 9999)) .
+                '.' . $request->file('avatar')->getClientOriginalExtension();
+            $request->file('avatar')->move('./public', $path);
+
+            $data = $request->all();
+            $data['avatar'] = './public' . $path;
+            try {
+                $teacher =  Teacher::where('id', $data['id'])->update([
+                    't_url' => $data['avatar'],
+                    'priority' => $data['priority'],
+                    'name' => $data['name'],
+                ]);
+            } catch (\Exception $e) {
+                logError('错误',[$e->getMessage()]);
+            }
+            return $teacher;
+        }
+    }
+
+
+    public static function teacherDelete($teacher){
+        try {
+            $data = Teacher::where('id', $teacher)->delete();
+        } catch (\Exception $e) {
+            logError('错误',[$e->getMessage()]);
+        }
+        return $data;
+    }
+
+
+    public static function teacherShow(){
+        $teacher = Teacher::select(['name','t_url','profession'])->paginate(6);
+        return $teacher;
+    }
+
+    public  static function teacherBack($id){
+
+        return  Teacher::where('id',$id)->pluck('t_url','priority','name')->first();
+
+    }
+
+
+    //指导老师
+    public static function guidTeacher(){
+        try{
+            $result = self::select('name','t_url','profession')->get();
+            //dd($result);
+            return $result;
+        } catch (\Exception $e){
+            logError('获取失败', [$e->getMessage()]);
+            return null;
+        }
+    }
 }
